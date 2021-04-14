1423bd4c91cd3e774eb309f7829b6b03b2885bb8
Liza Baez Kemp <baezkemp@gmail.com>
2021-04-14 16:26:36 -0500

    Update welcomeUserFromSession

--- src/main/java/com/example/burrowwebapp/controller/HelloController.java
+++ src/main/java/com/example/burrowwebapp/controller/HelloController.java
@@ -43,7 +43,7 @@ public class HelloController {
 
     private static final String userSessionKey = "user";
 
-    @RequestMapping(path = {"/", ""})
+    @RequestMapping("")
     public String welcomeUserFromSession(Model model, HttpSession session) {
         Integer userId = (Integer) session.getAttribute(userSessionKey);
         if (userId == null) {
